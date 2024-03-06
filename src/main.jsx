03b24446e5ee90a188a55673c0cee6a109908c71
dibborah <dibyajyotiborah469@gmail.com>
2024-03-06 21:25:00 +0530

    test

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
+//test
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
